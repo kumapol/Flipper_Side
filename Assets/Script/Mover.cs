d56ddf07186d9f6f6354a7cc17f636e744330442
kumapol <kubapolny@gmail.com>
2021-11-23 23:28:41 +0100

    Fiorst big commit

---
+++ Assets/Script/Mover.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField]  float speed=10f;
+  
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue=Input.GetAxis("Horizontal")*speed*Time.deltaTime;
+        float zValue=Input.GetAxis("Vertical")*speed*Time.deltaTime;
+        transform.Translate(xValue,0,zValue);
+         
+    }
+}
