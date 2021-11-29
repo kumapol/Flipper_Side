74e5181651f853400106afdd86b320aa83946866
kumapol <kubapolny@gmail.com>
2021-11-29 19:39:44 +0100

    Adding walls + player mesasge in log

--- Assets/Script/Mover.cs
+++ Assets/Script/Mover.cs
@@ -11,15 +11,28 @@ public class Mover : MonoBehaviour
 
     void Start()
     {
-
+        SendInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Moveplayer();
+    }
+
+    void SendInstructions()
+    {
+        Debug.Log("You controll player with arrows");
+
+    }
+
+    void Moveplayer()
+    {
         float xValue=Input.GetAxis("Horizontal")*speed*Time.deltaTime;
         float zValue=Input.GetAxis("Vertical")*speed*Time.deltaTime;
         transform.Translate(xValue,0,zValue);
-         
     }
+
+
+
 }
