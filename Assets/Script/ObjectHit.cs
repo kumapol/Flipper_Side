31cf175b03fb50e4d3724cb2c0df04f98217ee7e
kumapol <kubapolny@gmail.com>
2021-12-13 21:49:48 +0100

    Bassic collider actions + materials

---
+++ Assets/Script/ObjectHit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+ 
+ 
+  public  void OnCollisionEnter(Collision other) {
+           Debug.Log("Wall hit!");
+           GetComponent<MeshRenderer>().material.color=Color.red;
+           StartCoroutine(Uderzenie());
+  }
+  
+IEnumerator Uderzenie(){
+ yield return new WaitForSeconds(1);
+  GetComponent<MeshRenderer>().material.color=Color.green;
+}
+  
+  void Update()
+    {
+     
+      
+    }
+}
